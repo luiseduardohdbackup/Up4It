daa74ab4472f633a014142505de8e32d7dd5f964
jensendp <jensendp@kreatived.com>
2014-06-09 22:00:33 -0500

    Initial Commit

---
+++ Up4It/Up4It.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Up4It.WinPhone.Resources;
+
+namespace Up4It.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
